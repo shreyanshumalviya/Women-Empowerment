00b73d4a2b0c40cdc7ef5bf5efdaf4bce42707ba
Shreyanshu <shreyanshumalviya@gmail.com>
2022-07-18 23:13:44 +0530

    created ngo-home component

---
+++ women-empowerment/src/app/ngo-home/ngo-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngo-home',
+  templateUrl: './ngo-home.component.html',
+  styleUrls: ['./ngo-home.component.css']
+})
+export class NgoHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
