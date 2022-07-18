79b5a3c89c861349ad41ec38ceb845f7dd13defb
Shreyanshu <shreyanshumalviya@gmail.com>
2022-07-18 23:30:28 +0530

    created ngo login component

---
+++ women-empowerment/src/app/ngo/ngo-login/ngo-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngo-login',
+  templateUrl: './ngo-login.component.html',
+  styleUrls: ['./ngo-login.component.css']
+})
+export class NgoLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
