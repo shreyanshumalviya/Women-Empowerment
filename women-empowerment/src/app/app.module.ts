81bb2743a4fffbc21aa76827a454f6a7e441b1bc
Shreyanshu <shreyanshumalviya@gmail.com>
2022-07-18 23:29:58 +0530

    created parent ngo component

--- women-empowerment/src/app/app.module.ts
+++ women-empowerment/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgoHomeComponent } from './ngo-home/ngo-home.component';
+import { NgoHomeComponent } from './ngo/ngo-home/ngo-home.component';
+import { NgoLoginComponent } from './ngo/ngo-login/ngo-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgoHomeComponent
+    NgoHomeComponent,
+    NgoLoginComponent
   ],
   imports: [
     BrowserModule,
