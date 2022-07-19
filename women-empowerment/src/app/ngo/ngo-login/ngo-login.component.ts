b4adf57f4b547a9a15918f71b32452fb0b0b7c24
Shreyanshu <shreyanshumalviya@gmail.com>
2022-07-20 01:17:54 +0530

    finished login component

--- women-empowerment/src/app/ngo/ngo-login/ngo-login.component.ts
+++ women-empowerment/src/app/ngo/ngo-login/ngo-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgoService } from '../ngo.service';
 
 @Component({
   selector: 'app-ngo-login',
@@ -7,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgoLoginComponent implements OnInit {
 
-  errorMessage:string = ""
-  ngo:any={}
+  errorMessage: string = ""
+  ngo: any = {}
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public login():void{}
+  public login(): void {
+    let successfulLogin: Boolean = new NgoService().login(this.ngo.username, this.ngo.password);
+    console.log(successfulLogin);
+    if (!successfulLogin) { this.errorMessage = "Incorrect username or password" }
+    else {
+      this.errorMessage = ""
+    }
+  }
 
 }
