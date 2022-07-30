bf8dd407c72fb89e39f9d22d220ebc098d60508d
Rama Krishna <ramakrishnakasagani999@gmail.com>
2022-07-30 13:35:31 +0530

    Validations

--- women-empowerment/src/app/sukanya/registration/registration.component.ts
+++ women-empowerment/src/app/sukanya/registration/registration.component.ts
@@ -9,12 +9,33 @@ import { SukanyaService } from 'src/app/sukanya.service';
 })
 export class RegistrationComponent implements OnInit {
  sukanyaRegistration:SukanyaRegistration=new SukanyaRegistration();
+ maxDate = new Date();
+ minDate=new Date();
+ errorMessage="";
 
   constructor(private sukanyaService:SukanyaService) { }
 
   ngOnInit(): void {
+    this.minDate.setFullYear(this.minDate.getFullYear() - 10);
   }
 register(){
+  if(this.sukanyaRegistration.firstName===undefined || this.sukanyaRegistration.firstName===""){
+    this.errorMessage="Please Enter First Name";
+    return ;
+  }if(this.sukanyaRegistration.lastName===undefined || this.sukanyaRegistration.lastName===""){
+    this.errorMessage="Please Enter Last Name";
+    return ;
+  }
+  if(this.sukanyaRegistration.dateOfBirth===undefined){
+    this.errorMessage="Please Date of Birth";
+    return ;
+  }
+  if(this.sukanyaRegistration.aadharNumber===undefined || this.sukanyaRegistration.aadharNumber==="" ||this.sukanyaRegistration.aadharNumber.length<12){
+    this.errorMessage="Please Enter Valid Aadhar Number";
+    return ;
+  }
+
+
   console.log("Submit");
   this.sukanyaService.register(this.sukanyaRegistration)
   .subscribe(msg=>{console.log(msg);
