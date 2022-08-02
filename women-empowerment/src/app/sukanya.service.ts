0dccfdc5428609e5e0723113667e23d3434e9ea0
Shreyanshu <shreyanshumalviya@gmail.com>
2022-08-02 13:31:51 +0530

    Sukanya doc upload

--- women-empowerment/src/app/sukanya.service.ts
+++ women-empowerment/src/app/sukanya.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { SukanyaDoc } from './models/sukanya-doc';
+import { SukanyaRegResponse } from './models/sukanya-reg-response';
 import { SukanyaRegistration } from './models/sukanya-registration';
 
 @Injectable({
@@ -10,17 +12,21 @@ export class SukanyaService {
 
   constructor(private httpClient:HttpClient) { }
 
-  public register(sukanyaRegistration:SukanyaRegistration):Observable<String>{    
+  public register(sukanyaRegistration:SukanyaRegistration):Observable<SukanyaRegResponse>{    
     console.log(sukanyaRegistration.firstName);
     console.log(sukanyaRegistration.lastName);
     console.log(sukanyaRegistration.aadharNumber);
     console.log(sukanyaRegistration.dateOfBirth);
     console.log(JSON.stringify(sukanyaRegistration));
     
-    return this.httpClient.post<String>("http://localhost:9090/sukanya/register",sukanyaRegistration)
+    return this.httpClient.post<SukanyaRegResponse>("http://localhost:9090/sukanya/register",sukanyaRegistration)
     
     
     
     
+  }
+
+  public addSukanyaDoc(sukanyaDoc: SukanyaDoc):Observable<SukanyaDoc>{
+    return this.httpClient.post<SukanyaDoc>("http://localhost:9090/sukanya/upload_doc",sukanyaDoc)
   }
 }
